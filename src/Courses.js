f8155e4914ebf7a5dfda8918ea5bb3838cbd27d5
“Brian <brianlens@gmail.com>
2016-04-26 10:09:53 +0200

    initial react works

---
+++ src/Courses.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Courses extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        courses: []
+      };
+     }
+
+componentDidMount() {
+  this.setState({
+    courses: [{name: 'React', description: 'Learn to type fast with React'},
+              {name: 'JavaScript', description: 'Learn the basics'}
+              ]
+    });
+  }
+
+render(){
+  let courses = this.state.courses.map(function(course) {
+    return <li key={course.id}><h1>{course.name}</h1><p>{course.description}</p></li>;
+  });
+    return (
+      <ul>
+        {courses}
+      </ul>
+    )
+ }
+}
+
+export default Courses;
